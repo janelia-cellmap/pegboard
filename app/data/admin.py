610e20c83b96fbc57cdeeab3c9e503f51b4ce383
Jody Clements <jody@neomorphic.co.uk>
2022-02-04 14:19:37 -0500

    Adds filters and data prefetch to admin page.

--- app/data/admin.py
+++ app/data/admin.py
@@ -4,6 +4,9 @@ from .models import Record, Tissue, Crop
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('tissue', 'created', 'last_updated', 'crop')
+    list_filter = ('tissue', )
+    list_select_related = ('tissue', 'crop')
+    search_fields = ('tissue__short_name', 'crop__short_name')
 
 admin.site.register(Record, RecordAdmin)
 admin.site.register(Tissue)
